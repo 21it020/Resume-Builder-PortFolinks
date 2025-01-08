10042efb6810ca6343c90bcd86a287bf250cf07c
saral-ast <saralchauhan.ast@gmail.com>
2025-01-08 16:12:27 +0530

    commit

---
+++ frontend/src/components/custom/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react'
+
+function Header() {
+    const { user, isSignedIn } = useUser();
+    return (
+        <div className='p-3 px-5 flex justify-between shadow-md'>
+             <Link to={'/dashboard'}>
+            <img src='/logo.svg' className='cursor-pointer' width={100} height={100} />
+            </Link>
+            <p class="text-3xl font-bold text-purple-200decoration-indigo-500 hover:text-blue-800 transition duration-300">
+  PortFolinks
+</p>
+
+            {isSignedIn ?
+                <div className='flex gap-2 items-center'>
+                    <Link to={'/dashboard'}>
+                        <Button variant="outline">Dashboard</Button>
+                    </Link>
+                    <UserButton />
+                </div> :
+                <Link to={'/auth/sign-in'}>
+                    <Button>Get Started</Button>
+                </Link>
+            }
+
+        </div>
+    )
+}
+
+export default Header
