24fd15aa9006c6864f2499cda24d38a58aa4c173
saral-ast <saralchauhan.ast@gmail.com>
2025-01-08 16:03:35 +0530

    commit

---
+++ admin/src/api/user-resumes123/controllers/user-resumes123.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-resumes123 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-resumes123.user-resumes123');
---
+++ admin/src/api/user-resumes123/services/user-resumes123.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-resumes123 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::user-resumes123.user-resumes123');
