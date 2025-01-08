c66c6f6a6c8d5b28d2f40261c8b00a20efa45758
saral-ast <saralchauhan.ast@gmail.com>
2025-01-08 16:11:01 +0530

    commit

---
+++ admin/src/api/user-resumes123/routes/user-resumes123.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-resumes123 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-resumes123.user-resumes123');
